1a5c983422ebe92086c8601664b231c8e1de7328
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2025-12-05 06:53:03 +0000

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { Toaster } from 'sonner'
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <Routes>
           {/* Public Routes */}
